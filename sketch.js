23d9cf4ef1e06ad708305f2a1fccb87bd36047a3
MaxenceF96 <71009938+MaxenceF96@users.noreply.github.com>
2022-05-21 08:54:10 +0200

    Add files via upload
    2022, May 21, Test Version 6
    
    Changes :
    
    - Correction

--- sketch.js
+++ sketch.js
@@ -1040,13 +1040,14 @@ function drag_closest(){
   for (let i = 0; i < points_number; i++){
     if (points[i].type == 'selected'){
       points[i].type = 'normal'
-    } 
+    }
     if (points[i].type == 'selected_origin'){
       points[i].type = 'origin'
     }
     if (points[i].type == 'selected_scale'){
       points[i].type = 'scale'
     }
+  }
   closest();
   for (let i = 0; i < points_number; i++){
     if (points[i].type == 'selected'){
